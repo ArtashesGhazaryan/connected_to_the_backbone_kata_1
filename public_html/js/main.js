400af38379cc93cb78ebf2ae455c5e2b58a6c25e
vgrigoryan <vahagn.grigoryan@learnship.de>
2013-11-14 07:53:30 +0400

    added form view

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -7,7 +7,7 @@ var Tasky = {
     View: Backbone.View.extend({
         tagName: 'li',
         template: '#newTaskTemplate',
-        render: function(){
+        render: function () {
 
             var tplFnc = _.template($(this.template).html().trim());
             this.$el.html(
@@ -22,41 +22,57 @@ var Tasky = {
     }),
     CollectionView: Backbone.View.extend({
         tagName: 'ul',
-        events: {
-            'click form input[type="submit"]': 'createNewTask'
-        },
-        addTask: function(newTaskTitle){
+        addTask: function (newTaskTitle) {
             this.collection.add({title: newTaskTitle});
 
             return this;
         },
-        createNewTask: function(e, attrs){
-alert('asas');
-            e.preventDefault();
-
-            window.console.info(attrs);
-
-            return this;
-        },
-        render: function(){
-            this.collection.each(function(task){
+        render: function () {
+            this.collection.each(function (task) {
 
                 var newTask = new Tasky.View({model: task});
                 newTask.render();
-                
+
                 this.$el.append(newTask.$el);
             }, this);
 
-            $('body div').html(this.$el);
+//            $('body div').html(this.$el);
+
+            return this;
+        }
+    }),
+    FormView: Backbone.View.extend({
+        tagName: 'form',
+        template: '#formTemplate',
+        events: {
+            'click input[type="submit"]': 'createNewTask'
+        },
+        createNewTask: function (e, attrs) {
+
+            e.preventDefault();
+            window.console.info('task was created');
+            return this;
+        },
+        render: function () {
+            var tplFnc = _.template($(this.template).html().trim());
+
+            $('body').html(
+                this.$el.html(
+                    tplFnc()
+                )
+            );
 
             return this;
         }
     })
 };
 
-var taskCollection = new Tasky.Collection();
-var taskCollectionView = new Tasky.CollectionView({collection: taskCollection});
+//var taskCollection = new Tasky.Collection();
+//var taskCollectionView = new Tasky.CollectionView({collection: taskCollection});
+//
+//taskCollectionView.addTask('gnal gorci');
+//taskCollectionView.addTask('gal tun');
+//taskCollectionView.render();
 
-taskCollectionView.addTask('gnal gorci');
-taskCollectionView.addTask('gal tun');
-taskCollectionView.render();
+var taskForm = new Tasky.FormView();
+taskForm.render();
